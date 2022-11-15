0b38ca72442dcc8c75f862ed739f2b702cb64a12
eunseolYun <skopdt73@gmail.com>
2022-11-15 17:07:46 +0900

    map unique key prop, proptypes : props를 제대로 보내고 있는지 확인

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-function Food({ name, country }) {
+import PropTypes from "prop-types";
+
+function Food({ name, country, rating }) {
   return (
     <div>
       <h3>
         {name} in {country}
       </h3>
+      <h4>{rating}/5.0</h4>
     </div>
   );
 }
@@ -11,19 +14,33 @@ function Food({ name, country }) {
 // App에서 child-compo로 정보를 보내고 정보를 사용하는 법
 // property으로 value를 보냄(log porps => Object) like HTML: jsx
 // ex) <Food fav="kimbab" />  =>여러정보를 보내기에 비효율적
+// list의 child에는 각각의 unique key prop이 있어야한다.
+// props를 제대로 보내고 있는지 확인 ⇒ PropTypes
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 const foodILike = [
   {
+    id: 1,
     name: "kimbab",
     country: "korea",
+    rating: 4.5,
   },
   {
+    id: 2,
     name: "pizza",
     country: "italia",
+    rating: 5.0,
   },
   {
+    id: 3,
     name: "donuts",
-    country: "usa",
+    country: "U.S.A",
+    rating: 4.0,
   },
 ];
 
@@ -32,7 +49,12 @@ function App() {
     <div>
       <h1>Food Contest List</h1>
       {foodILike.map((dish) => (
-        <Food name={dish.name} country={dish.country} />
+        <Food
+          key={dish.id} // react need unique key
+          name={dish.name}
+          country={dish.country}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
