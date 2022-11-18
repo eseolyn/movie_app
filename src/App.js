c2c7ed9f3443f5621607772ae37406f22c53aede
eunseolYun <skopdt73@gmail.com>
2022-11-18 13:27:59 +0900

    example react Component life cycle

--- src/App.js
+++ src/App.js
@@ -1,63 +1,28 @@
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
+import React from "react";
 
-function Food({ name, country, rating }) {
-  return (
-    <div>
-      <h3>
-        {name} in {country}
-      </h3>
-      <h4>{rating}/5.0</h4>
-    </div>
-  );
-}
-// component file&function는 대문자
-// App에서 child-compo로 정보를 보내고 정보를 사용하는 법
-// property으로 value를 보냄(log porps => Object) like HTML: jsx
-// ex) <Food fav="kimbab" />  =>여러정보를 보내기에 비효율적
-// list의 child에는 각각의 unique key prop이 있어야한다.
-// props를 제대로 보내고 있는지 확인 ⇒ PropTypes
+// react can save dynamic data in state
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  country: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-const foodILike = [
-  {
-    id: 1,
-    name: "kimbab",
-    country: "korea",
-    rating: 4.5,
-  },
-  {
-    id: 2,
-    name: "pizza",
-    country: "italia",
-    rating: 5.0,
-  },
-  {
-    id: 3,
-    name: "donuts",
-    country: "U.S.A",
-    rating: 4.0,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      <h1>Food Contest List</h1>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id} // react need unique key
-          name={dish.name}
-          country={dish.country}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  // 외부 상태에 의존하지 않도록 use ‘current’
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
