d8a14fa31a84a6e3d2f56b5b19e5679099dd1bba
eunseolYun <skopdt73@gmail.com>
2022-12-12 18:38:34 +0900

    react movie webapp
    2021ver lecture
    컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
-//import PropTypes from "prop-types";
-import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
+import Button from "./Button";
+import styles from "./App.module.css";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false });
-  };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader-text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <h1 className={styles.title}>Welcome to React!</h1>
+      <Button text={"continue"} />
+    </div>
+  );
 }
 
 export default App;
